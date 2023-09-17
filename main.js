961d2da43699a1d3d9b99b2ed04339a62efc7952
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-09-17 21:54:50 +0300

    Updated file

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-angular.module("app", []).controller("myCtrl", function ($scope) {
+app = angular.module("app", [])
+
+app.controller("myCtrl", function ($scope) {
     $scope.hello = 55
     $scope.laptop = "MacBook M1 pro"
+})
+
+app.controller("first", function ($scope, myFactory) {
+    $scope.myFactory = myFactory
+    $scope.oneHello = "hello world"
+})
+app.controller("second", function ($scope, myFactory) {
+    $scope.myFactory = myFactory
+    $scope.twoHello = "hello world"
+})
+
+app.factory("myFactory", function () {
+    return {
+        hello: "hello world"
+    }
 })
