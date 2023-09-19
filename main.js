6225483f472093ee3647633b36c129c2f0dfc8a9
zaytsev <zaicev@mniti.ru>
2023-09-19 12:35:28 +0300

    Directive part 2

--- main.js
+++ main.js
@@ -73,11 +73,15 @@ app.directive('func', function (){
 app.directive('func', function (){
     return{
         link: function (scope, element, attrs) {
-            console.log("This is my first directive")
+           /* console.log("This is my first directive")
             console.log('scope', scope)
             console.log('element', element)
             console.log('attrs', attrs)
-            element.text('This is my magic directive')
+            element.text('This is my magic directive')*/
+            element.on('click', function () {
+                console.log('click')
+                element.text() === "Func" ? element.text('Bar') : element.text('Func')
+            })
     }
     }
 })
